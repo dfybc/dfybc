7d55fbedba785fd6e66ebcd984875311ef9eb30b
dfybc <wangzongzheng@126.com>
2024-09-04 17:25:31 +0800

    增加配置项

---
+++ other/config.py
@@ -0,0 +1,14 @@
+# 百度翻译
+BAIDU_TRANSLATE_APP_ID = 'your appID'
+BAIDU_TRANSLATE_SECRET_KEY = 'your secret key'
+
+# 和风天气配置
+HEFENG_KEY = 'your key'
+
+# 百度大模型配置
+BAIDU_AIGC_API_KEY = "your api key "
+BAIDU_AIGC_SECRET_KEY = "your secret key"
+
+# 百度AI配置
+BAIDU_AI_API_KEY = "your api key"
+BAIDU_AI_SECRET_KEY = "your secret key"
