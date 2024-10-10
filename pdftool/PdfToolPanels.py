a4cfe749db688168794d680aaec1adc8671d690f
dfybc <wangzongzheng@126.com>
2024-10-10 09:00:40 +0800

    增加pdf拆分功能

--- pdftool/PdfToolPanels.py
+++ pdftool/PdfToolPanels.py
@@ -14,12 +14,13 @@ import wx
 import wx.xrc
 from pdf2image import convert_from_path
 import img2pdf
+import PyPDF2 
 
 _ = gettext.gettext
 
-NOT_CHOOSE_FILE_STR = '还未选择文件'
+NOT_CHOOSE_FILE_STR = '还未选择PDF文件'
 CONVERT_SUCCESS_STR = '转换成功'
-
+SPLIT_SUCCESS_PREFIX = '拆分成功，拆分文件为：'
 ###########################################################################
 ## Class Pdf2ImgPanel
 ###########################################################################
@@ -39,10 +40,10 @@ class Pdf2ImgPanel ( wx.Panel ):
 
         hbox1 = wx.BoxSizer( wx.HORIZONTAL )
 
-        self.btn_choosefile = wx.Button( self, wx.ID_ANY, _(u"点击选择文件"), wx.DefaultPosition, wx.DefaultSize, 0 )
+        self.btn_choosefile = wx.Button( self, wx.ID_ANY, _(u"点击选择PDF文件"), wx.DefaultPosition, wx.DefaultSize, 0 )
         hbox1.Add( self.btn_choosefile, 0, wx.ALL, 5 )
 
-        self.lbl_show_choose_file = wx.StaticText( self, wx.ID_ANY, _(u"还未选择文件"), wx.DefaultPosition, wx.DefaultSize, 0 )
+        self.lbl_show_choose_file = wx.StaticText( self, wx.ID_ANY, _(u"还未选择PDF文件"), wx.DefaultPosition, wx.DefaultSize, 0 )
         self.lbl_show_choose_file.Wrap( -1 )
 
         hbox1.Add( self.lbl_show_choose_file, 0, wx.ALIGN_CENTER|wx.ALL, 5 )
@@ -266,3 +267,169 @@ class Img2PdfPanel ( wx.Panel ):
         else:
             self.textctl_result.SetValue(self.result)
             self.textctl_result.SetForegroundColour('red')
+
+###########################################################################
+## Class PdfSplit
+###########################################################################
+
+class PdfSplit ( wx.Panel ):
+
+    def __init__( self, parent, id = wx.ID_ANY, pos = wx.DefaultPosition, size = wx.Size( 500,300 ), style = wx.TAB_TRAVERSAL, name = wx.EmptyString ):
+        wx.Panel.__init__ ( self, parent, id = id, pos = pos, size = size, style = style, name = name )
+
+        self.input_dir = ''
+        self.input_file_path = ''
+        self.output_dir = ''
+        self.page_num = 5
+        self.lbltxt_show_input_file_path = NOT_CHOOSE_FILE_STR
+        self.result = ''
+
+        vbox = wx.BoxSizer( wx.VERTICAL )
+
+        hbox1 = wx.BoxSizer( wx.HORIZONTAL )
+
+        self.btn_choosefile = wx.Button( self, wx.ID_ANY, _(u"点击选择PDF文件"), wx.DefaultPosition, wx.DefaultSize, 0 )
+        hbox1.Add( self.btn_choosefile, 0, wx.ALL, 5 )
+
+        self.lbl_show_choose_file = wx.StaticText( self, wx.ID_ANY, _(u"还未选择PDF文件"), wx.DefaultPosition, wx.DefaultSize, 0 )
+        self.lbl_show_choose_file.Wrap( -1 )
+
+        hbox1.Add( self.lbl_show_choose_file, 0, wx.ALIGN_CENTER|wx.ALL, 5 )
+
+
+        vbox.Add( hbox1, 0, wx.EXPAND, 5 )
+
+        bSizer18 = wx.BoxSizer( wx.HORIZONTAL )
+
+        self.m_staticText10 = wx.StaticText( self, wx.ID_ANY, _(u"每个拆分文件的页数"), wx.DefaultPosition, wx.DefaultSize, 0 )
+        self.m_staticText10.Wrap( -1 )
+
+        bSizer18.Add( self.m_staticText10, 0, wx.ALL, 5 )
+
+        self.spinCtrl_pagenum = wx.SpinCtrl( self, wx.ID_ANY, wx.EmptyString, wx.DefaultPosition, wx.DefaultSize, wx.SP_ARROW_KEYS, 0, 10, 5 )
+        bSizer18.Add( self.spinCtrl_pagenum, 0, wx.ALL, 5 )
+
+
+        vbox.Add( bSizer18, 0, wx.EXPAND, 5 )
+
+        hbox2 = wx.BoxSizer( wx.HORIZONTAL )
+
+        self.m_staticText2 = wx.StaticText( self, wx.ID_ANY, _(u"PDF文件拆分输出目录："), wx.DefaultPosition, wx.DefaultSize, 0 )
+        self.m_staticText2.Wrap( -1 )
+
+        hbox2.Add( self.m_staticText2, 0, wx.ALIGN_CENTER|wx.ALL, 5 )
+
+        self.textctl_outputdir = wx.TextCtrl( self, wx.ID_ANY, wx.EmptyString, wx.DefaultPosition, wx.DefaultSize, 0 )
+        hbox2.Add( self.textctl_outputdir, 0, wx.ALIGN_CENTER|wx.ALL, 5 )
+
+        self.btn_chdir = wx.Button( self, wx.ID_ANY, _(u"点击切换\nPDF拆分文件输出目录"), wx.DefaultPosition, wx.DefaultSize, 0 )
+        hbox2.Add( self.btn_chdir, 0, wx.ALL, 5 )
+
+
+        vbox.Add( hbox2, 0, wx.EXPAND, 5 )
+
+        hbox3 = wx.BoxSizer( wx.VERTICAL )
+
+        self.btn_convert = wx.Button( self, wx.ID_ANY, _(u"开始拆分"), wx.DefaultPosition, wx.DefaultSize, 0 )
+        hbox3.Add( self.btn_convert, 0, wx.ALL, 5 )
+
+        self.textcrl_result = wx.TextCtrl( self, wx.ID_ANY, wx.EmptyString, wx.DefaultPosition, wx.DefaultSize, wx.TE_MULTILINE|wx.TE_READONLY|wx.BORDER_NONE )
+        hbox3.Add( self.textcrl_result, 1, wx.ALL|wx.EXPAND, 5 )
+
+
+        vbox.Add( hbox3, 0, wx.EXPAND, 5 )
+
+
+        self.SetSizer( vbox )
+        self.Layout()
+
+        # Connect Events
+        self.btn_choosefile.Bind( wx.EVT_BUTTON, self.choose_file )
+        self.spinCtrl_pagenum.Bind( wx.EVT_SPINCTRL, self.chg_pagenum )
+        self.btn_chdir.Bind( wx.EVT_BUTTON, self.chg_output_dir )
+        self.btn_convert.Bind( wx.EVT_BUTTON, self.split )
+
+    def __del__( self ):
+        pass
+
+
+    # Virtual event handlers, override them in your derived class
+    def choose_file( self, event ):
+        dlg = wx.FileDialog(self, "选择文件", self.input_dir, "", "*.pdf;*.PDF", wx.FD_OPEN | wx.FD_FILE_MUST_EXIST)
+        if dlg.ShowModal() == wx.ID_OK:
+            self.input_file_path = dlg.GetPath()    # 获取文件完整路径
+            self.input_dir = dlg.GetDirectory()    # 获取文件目录
+            self.output_dir = ''
+            with open(self.input_file_path, 'rb') as f:
+                reader = PyPDF2.PdfReader(f)
+                self.lbltxt_show_input_file_path = f'已选择文件：{self.input_file_path}，共有{len(reader.pages)}页 '
+            self.result = ''
+            self.update()
+        dlg.Destroy()
+
+    def chg_pagenum( self, event ):
+        self.page_num = self.spinCtrl_pagenum.GetValue()
+        print(self.page_num)
+
+    def chg_output_dir( self, event ):
+        dlg = wx.DirDialog(self, "选择图片输出目录", self.input_dir, style=wx.DD_DEFAULT_STYLE | wx.DD_NEW_DIR_BUTTON)
+        if dlg.ShowModal() == wx.ID_OK:
+            self.output_dir = dlg.GetPath()
+            print(self.output_dir)
+            self.textctl_outputdir.SetValue(self.output_dir)
+        dlg.Destroy()
+
+    def split( self, event ):
+        try:
+            self.result = ''
+            if self.input_file_path == '':
+                self.result = '请先选择要拆分PDF文件'
+            elif self.output_dir == '':
+                self.result = '请先选择PDF拆分文件输出目录'
+            else:
+                with open(self.input_file_path, 'rb') as f: 
+                    reader = PyPDF2.PdfReader(f)  
+                    num_pages = len(reader.pages)
+            
+                    # 创建输出文件夹（如果不存在）  
+                    import os  
+                    if not os.path.exists(self.output_dir):  
+                        os.makedirs(self.output_dir)  
+            
+                    # 遍历PDF文件的每一页  
+                    for page_num in range(0, num_pages, self.page_num):  
+                        # 计算当前输出文件的页数范围  
+                        end = min(page_num + self.page_num, num_pages) 
+                        output_filename = f"output_{page_num // self.page_num + 1}.pdf" 
+                        output_filename_fullpath = os.path.join(self.output_dir, output_filename)  
+            
+                        # 创建PDF写入器  
+                        with open(output_filename_fullpath, 'wb') as out:  
+                            writer = PyPDF2.PdfWriter()  
+            
+                            # 复制指定范围的页面到新的PDF文件  
+                            for page_num_in_range in range(page_num, end):  
+                                page = reader.pages[page_num_in_range]
+                                writer.add_page(page)  
+            
+                            # 写入新的PDF文件  
+                            writer.write(out)  
+                            self.result += f'，{output_filename}'
+                    self.result = SPLIT_SUCCESS_PREFIX + self.result
+        except Exception as e:  
+            self.result = f"Error: {str(e)}"
+
+        self.update_result()
+
+    def update_result(self):
+        if self.result.find('成功') != -1:
+            self.textcrl_result.SetValue(self.result)
+            self.textcrl_result.SetForegroundColour('blue')
+        else:
+            self.textcrl_result.SetValue(self.result)
+            self.textcrl_result.SetForegroundColour('red')
+
+    def update(self):
+        self.lbl_show_choose_file.SetLabel(self.lbltxt_show_input_file_path)
+        self.textctl_outputdir.SetValue('')
+        self.update_result()
--- pdftool/main.py
+++ pdftool/main.py
@@ -1,5 +1,5 @@
 import wx
-from PdfToolPanels import Pdf2ImgPanel, Img2PdfPanel
+from PdfToolPanels import Pdf2ImgPanel, Img2PdfPanel, PdfSplit
 
 app = wx.App()
 frame = wx.Frame(None, title='PDF小工具', size=(600, 400))
@@ -7,6 +7,7 @@ notebook = wx.Notebook(frame)
 
 notebook.AddPage(Pdf2ImgPanel(notebook), "PDF转图片")
 notebook.AddPage(Img2PdfPanel(notebook), "图片合成PDF")
+notebook.AddPage(PdfSplit(notebook), "PDF拆分")
 
 frame.Show()
 app.MainLoop()
